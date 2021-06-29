ae658c59dfa6c6f6ec789c36582b71a8d8b3b6da
SmartMadique <79640865+SmartMadique@users.noreply.github.com>
2021-06-29 17:43:48 +0600

    Add files via upload

---
+++ scrollLabel.py
@@ -0,0 +1,26 @@
+# Модуль с классом-наследником ScrollView для создания инструкции, у которой при необходимости включается полоса прокрутки
+
+# Здесь должен быть твой код
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.label import Label
+
+class ScrollLabel(ScrollView):
+    def __init__(self, ltext, textcolor = '#d1af3f', **kwargs):
+        super().__init__(**kwargs)
+
+        self.textcolor = textcolor
+        ftext = '[color=' + self.textcolor + ']' + ltext + '[/color]'
+
+        self.label = Label(text=ftext, markup=True, size_hint_y=None, font_size='18sp', halign='left', valign='top')
+        self.label.bind(size=self.resize)
+        self.add_widget(self.label)
+
+    def resize(self, *args):
+        self.label.text_size = (self.label.width, None)
+        self.label.texture_update()
+        self.label.height = self.label.texture_size[1]
+
+    def set_text(self, ltext):
+        ftext = '[color=' + self.textcolor + ']' + ltext + '[/color]'
+        self.label.text = ftext
+        self.resize()
---
+++ sits.py
@@ -0,0 +1,17 @@
+# Модуль для вычисления количества приседаний
+
+# Здесь должен быть твой код
+from scrollLabel import ScrollLabel
+
+class Sits(ScrollLabel):
+    def __init__(self, total, **kwargs):
+        self.current = 0
+        self.total = total
+        text = "Осталось приседаний: [b]" + str(self.total) + "[/b]"
+        super().__init__(text, **kwargs)
+
+    def next(self, *args):
+        self.current += 1
+        remain = max(0, self.total - self.current)
+        text = "Осталось приседаний: [b]" + str(remain) + "[/b]"
+        super().set_text(text)
